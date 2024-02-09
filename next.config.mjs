8130a424723248d8c5948804d8d7150665034a58
ryzhaknode <denryzhak@gmail.com>
2024-02-09 14:09:23 +0200

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    // Додайте конфігурацію для експорту
-    exportPathMap: function () {
-        return {
-            '/': {page: '/'},
-            // Додайте інші маршрути за необхідністю
-        };
-    },
-};
+    output: 'export',
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+}
+
 
 export default nextConfig;
