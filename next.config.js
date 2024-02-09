4b65364d54802b719cc2c88a345ba3cb819a422f
ryzhaknode <denryzhak@gmail.com>
2024-02-09 14:48:48 +0200

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    reactStrictMode: true,
     output: 'export',
+    reactStrictMode: true,
     images: {
         unoptimized: true,
     },
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
 }
 
 module.exports = nextConfig
